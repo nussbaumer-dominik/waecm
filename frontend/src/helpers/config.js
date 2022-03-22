da1774d902ccd3fb6ddf3e1588e23c740f773dd4
Dominik Nußbaumer <nussbaumer.dominik@pm.me>
2022-03-22 17:36:33 +0100

    changed port to 4444

--- frontend/src/helpers/config.js
+++ frontend/src/helpers/config.js
@@ -1,7 +1,7 @@
 const config = {
   clientId: "waecm",
   OPServer: "https://waecm-sso.inso.tuwien.ac.at/realms/waecm",
-  redirectURL: "http://localhost:4200/callback",
+  redirectURL: "http://localhost:4444/callback",
   scope: "openid profile",
   userInfoEndpoint: "/protocol/openid-connect/userinfo",
   introspectionEndpoint: "/protocol/openid-connect/userinfo",
