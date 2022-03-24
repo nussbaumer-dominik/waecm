0e30884a2159f60141f53d26dc01a6004d2764b3
Dominik Nußbaumer <nussbaumer.dominik@pm.me>
2022-03-24 15:20:27 +0100

    refactored

--- frontend/src/components/Buttons.jsx
+++ frontend/src/components/Buttons.jsx
@@ -10,7 +10,7 @@ export default function Buttons(props) {
         <button className="btn btn-secondary btn-getuser" style={{margin: "10px"}} onClick={props.getUser}>
           Get User info
         </button>
-        <button className="btn btn-success btn-getapi" style={{margin: "10px"}} onClick={props.callApi}>
+        <button className="btn btn-warning btn-getapi" style={{margin: "10px"}} onClick={props.callApi}>
           Call Backend
         </button>
         <button className="btn btn-success btn-renewtoken" style={{margin: "10px"}} onClick={props.renewToken}>
