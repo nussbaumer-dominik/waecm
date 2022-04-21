8aaf56ac260d6dc5c1b68f20a695255e691cdfaa
Dominik Nußbaumer <nussbaumer.dominik@pm.me>
2022-04-21 18:15:35 +0200

    completely reworked structure

---
+++ frontend/src/components/History.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default function History(props) {
+  return (
+    <div className="row">
+      <div className="col">
+        <h2 className="text-center">History Komponente</h2>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/components/Settings.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default function Settings(props) {
+  return (
+    <div className="row">
+      <div className="col">
+        <h2 className="text-center">Settings Komponente</h2>
+      </div>
+    </div>
+  );
+}
