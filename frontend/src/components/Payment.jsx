ef5961f5b1ec7fc4548df0270780cc7267b59227
Dominik Nußbaumer <nussbaumer.dominik@pm.me>
2022-04-22 15:30:47 +0200

    added temp styling for payment component

--- frontend/src/components/Payment.jsx
+++ frontend/src/components/Payment.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Col, Row} from "react-bootstrap";
+import {Button, Card, Col, Form, Row} from "react-bootstrap";
 
 export default function Payment(props) {
   if (props.user == null) {
@@ -7,9 +7,31 @@ export default function Payment(props) {
   }
 
   return (
-    <Row>
-      <Col>
-        <h2 className="text-center">Payment Komponente</h2>
+    <Row className="justify-content-center">
+      <Col className="col-6">
+        <Card>
+          <Card.Header className="text-center">
+            Neue Zahlung
+          </Card.Header>
+          <Card.Body>
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Label>Betrag</Form.Label>
+                <Form.Control type="number" placeholder="€ Betrag" />
+                <Form.Text className="text-muted float-right">X SAT</Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Beschreibung (Optional)</Form.Label>
+                <Form.Control type="text" placeholder="Beschreibung" />
+                <Form.Text className="text-muted float-right">Noch 30 Zeichen</Form.Text>
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
       </Col>
     </Row>
   );
