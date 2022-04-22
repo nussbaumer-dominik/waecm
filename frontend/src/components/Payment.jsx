5d5617f972b0db193e8a074c5e92c7b4da6054a0
Dominik Nußbaumer <nussbaumer.dominik@pm.me>
2022-04-22 16:24:43 +0200

    updated profile and payment components

--- frontend/src/components/Payment.jsx
+++ frontend/src/components/Payment.jsx
@@ -1,9 +1,42 @@
 import * as React from "react";
+import {useState} from "react";
 import {Button, Card, Col, Form, Row} from "react-bootstrap";
 
 export default function Payment(props) {
   if (props.user == null) {
     window.location = "/";
+  }
+
+  const [paymentInfo, setPaymentInfo] = useState({
+    amount: 0,
+    description: ""
+  })
+
+
+  function convertBTCtoSatoshi(amount) {
+    return amount / 100000000;
+  }
+
+  const handleAmountChange = event => {
+    setPaymentInfo({...paymentInfo, amount: parseFloat(event.target.value)});
+  }
+
+  const handleDescriptionChange = event => {
+    if (event.target.value.length <= 30 ) {
+      setPaymentInfo({...paymentInfo, description: event.target.value});
+    }
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(paymentInfo);
+    if (paymentInfo.amount === 0 || paymentInfo.amount == null) {
+      console.error("Der Betrag ist ein Pflichtfeld!")
+      alert("Der Betrag ist ein Pflichtfeld!");
+      return;
+    } else {
+      alert(JSON.stringify(paymentInfo));
+    }
   }
 
   return (
@@ -14,20 +47,29 @@ export default function Payment(props) {
             Neue Zahlung
           </Card.Header>
           <Card.Body>
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <Form.Group className="mb-3">
-                <Form.Label>Betrag</Form.Label>
-                <Form.Control type="number" placeholder="€ Betrag" />
-                <Form.Text className="text-muted float-right">X SAT</Form.Text>
+                <Form.Label>Betrag (in €)</Form.Label>
+                <Form.Control type="number"
+                              name="amount"
+                              placeholder="€ Betrag"
+                              value={paymentInfo.amount}
+                              onChange={handleAmountChange} />
+                <Form.Text className="text-muted float-right">{convertBTCtoSatoshi(paymentInfo.amount)} SAT</Form.Text>
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Beschreibung (Optional)</Form.Label>
-                <Form.Control type="text" placeholder="Beschreibung" />
-                <Form.Text className="text-muted float-right">Noch 30 Zeichen</Form.Text>
+                <Form.Label>Beschreibung <span className="text-muted">(Optional)</span></Form.Label>
+                <Form.Control type="text"
+                              name="description"
+                              placeholder="Beschreibung"
+                              maxLength="30"
+                              value={paymentInfo.description}
+                              onChange={handleDescriptionChange}/>
+                <Form.Text className="text-muted float-right">Noch {30 - paymentInfo.description.length} Zeichen</Form.Text>
               </Form.Group>
               <Button variant="primary" type="submit">
-                Submit
+                Bezahlen
               </Button>
             </Form>
           </Card.Body>
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,69 +1,82 @@
 import * as React from "react";
-import { Row, Col } from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 
 export default function Profile(props) {
-    if (props.user == null) {
-        window.location = "/";
-    }
+  if (props.user == null) {
+    window.location = "/";
+  }
 
-    return (
-        <div>
-            <Row className="justify-content-center">
-                <Col va>
-                    <img src={props.user.profile.picture} alt="avatar" width="128" height="128" className="rounded-circle" />
-                </Col>
-            </Row>
-            <Row className="justify-content-center">
-                <Col>
-                    <div className="card text-white bg-success mb-3">
-                        <div className="card-body">
-                            <h5 className="card-title">Userinfo</h5>
-                            <dl className="row">
-                                <dt className="col-sm-2">access_token</dt>
-                                <dd className="col-sm-10">
-                                    {props.user.access_token}
-                                </dd>
+  return (
+    <Container>
+      <Row className="justify-content-center">
+        <Col className="col-md-6 ml-auto mr-auto">
+          <div className="profile d-flex align-items-center flex-column">
+            <img src={props.user.profile.picture} alt="avatar" width="128" height="128"
+                 className="img-raised rounded-circle"/>
+            <div className="name">
+              <h3 className="title mb-0">{props.user.profile.name}</h3>
+              <span className="username text-muted">@{props.user.profile.preferred_username}</span>
+            </div>
+          </div>
 
-                                <dt className="col-sm-2">refresh_token</dt>
-                                <dd className="col-sm-10">
-                                    {props.user.refresh_token}
-                                </dd>
+        </Col>
+      </Row>
 
-                                <dt className="col-sm-2">sub</dt>
-                                <dd className="col-sm-10">
-                                    {props.user.profile.sub}
-                                </dd>
+      <Row className="justify-content-center">
+        <Col>
+          <div className="card text-white bg-success mb-3">
+            <div className="card-body">
+              <h5 className="card-title">Userinfo</h5>
+              <dl className="row">
+                <dt className="col-sm-2">access_token</dt>
+                <dd className="col-sm-10">
+                  {props.user.access_token}
+                </dd>
 
-                                <dt className="col-sm-2">email verified</dt>
-                                <dd className="col-sm-10">
-                                    {props.user.profile.email_verified ? "yes" : "no"}
-                                </dd>
+                <dt className="col-sm-2">refresh_token</dt>
+                <dd className="col-sm-10">
+                  {props.user.refresh_token}
+                </dd>
 
-                                <dt className="col-sm-2">name</dt>
-                                <dd className="col-sm-10">
-                                    {props.user.profile.name}
-                                </dd>
+                <dt className="col-sm-2">sub</dt>
+                <dd className="col-sm-10">
+                  {props.user.profile.sub}
+                </dd>
 
-                                <dt className="col-sm-2">preferred_username</dt>
-                                <dd className="col-sm-10">
-                                    {props.user.profile.preferred_username}
-                                </dd>
+                <dt className="col-sm-2">email verified</dt>
+                <dd className="col-sm-10">
+                  {props.user.profile.email_verified ? "yes" : "no"}
+                </dd>
 
-                                <dt className="col-sm-2">given_name</dt>
-                                <dd className="col-sm-10">
-                                    {props.user.profile.given_name}
-                                </dd>
+                <dt className="col-sm-2">name</dt>
+                <dd className="col-sm-10">
+                  {props.user.profile.name}
+                </dd>
 
-                                <dt className="col-sm-2">family_name</dt>
-                                <dd className="col-sm-10">
-                                    {props.user.profile.family_name}
-                                </dd>
-                            </dl>
-                        </div>
-                    </div>
+                <dt className="col-sm-2">preferred_username</dt>
+                <dd className="col-sm-10">
+                  {props.user.profile.preferred_username}
+                </dd>
 
-                </Col>
-            </Row>
-        </div>
-    );
+                <dt className="col-sm-2">given_name</dt>
+                <dd className="col-sm-10">
+                  {props.user.profile.given_name}
+                </dd>
+
+                <dt className="col-sm-2">family_name</dt>
+                <dd className="col-sm-10">
+                  {props.user.profile.family_name}
+                </dd>
+              </dl>
+            </div>
+          </div>
+
+        </Col>
+      </Row>
+    </Container>
+  );
+
 }
+
+
+
