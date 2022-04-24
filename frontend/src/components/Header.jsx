d42e5cb8ade9fd2222972f9a6fd1d3c102fb5b9c
Dominik Nußbaumer <nussbaumer.dominik@pm.me>
2022-04-24 11:54:02 +0200

    removed top margin from header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,7 +7,7 @@ import DropdownItem from "react-bootstrap/DropdownItem";
 
 export default function Header(props) {
   return (
-    <header className="my-3 border-bottom">
+    <header className="mb-3 border-bottom">
       <div className="container">
         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
           <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
